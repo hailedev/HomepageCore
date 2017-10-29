167d7b7156bcfe1135c5ee69610fae63f1b674b2
hailedev <hai.le@hotmail.com.au>
2017-10-29 01:13:11 -0700

    more prerendering stuff

--- HomepageCore.UI/ClientApp/js/app.jsx
+++ HomepageCore.UI/ClientApp/js/app.jsx
@@ -27,36 +27,15 @@ require("whatwg-fetch");
 var React = require("react");
 var ReactDOM = require("react-dom");
 var Router = require("react-router").Router;
-var Route = require("react-router").Route;
-var IndexRoute = require("react-router").IndexRoute;
 var browserHistory = require("react-router").browserHistory;
 
-var Main = require("./pages/Main");
-var HomeLandingPage = require("./pages/home/HomeLandingPage");
-var PostPage = require("./pages/post/PostPage");
-var AboutPage = require("./pages/about/AboutPage");
-var ResumePage = require("./pages/resume/ResumePage");
-var FeedbackPage = require("./pages/feedback/FeedbackPage");
-var ProjectsPage = require("./pages/projects/ProjectsPage");
-var AdminPage = require("./pages/admin/AdminPage");
-
 var browser = require("detect-browser");
+var Routes = require("./Routes");
+
 var onUpdate = function(){
     window.scrollTo(0, 0);
 };
-var pageContent = (
-    <Router history={browserHistory} onUpdate={onUpdate}>
-        <Route path="/" component={Main}>
-            <IndexRoute component={HomeLandingPage}/>
-            <Route path="about" component={AboutPage}/>
-            <Route path="hire-me" component={ResumePage}/>
-            <Route path="feedback" component={FeedbackPage}/>
-            <Route path="projects" component={ProjectsPage}/>
-            <Route path="post/:id" component={PostPage}/>
-            <Route path="admin" component={AdminPage}/>
-            <Route path="admin/:id" component={AdminPage}/>
-        </Route>
-    </Router>);
+var pageContent = <Router routes={Routes} history={browserHistory} onUpdate={onUpdate}/>;
 if(browser && browser.name === "ie" && !browser.version.startsWith("11")){
     pageContent = (
         <div className="container">
--- HomepageCore.UI/ClientApp/js/prerender-bootstrapper.js
+++ HomepageCore.UI/ClientApp/js/prerender-bootstrapper.js
@@ -1,7 +1,10 @@
 var prerendering = require('aspnet-prerendering');
+var prerenderService = require("./prerender-app");
 
 module.exports = prerendering.createServerRenderer(function(params) {
     return new Promise(function (resolve, reject) {
-        resolve({ html: require("./prerender-app") });
+        prerenderService(params.absoluteUrl).then(function(htmlOutput){
+            resolve({ html: htmlOutput });
+        });
     });
 });
