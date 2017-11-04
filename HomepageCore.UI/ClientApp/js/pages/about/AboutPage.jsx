3a66cfe73d56f2eb8e67bea7aab6f19e3fa3554d
hailedev <hai.le@hotmail.com.au>
2017-11-03 20:10:30 -0700

    fixes

--- HomepageCore.UI/ClientApp/js/pages/about/AboutPage.jsx
+++ HomepageCore.UI/ClientApp/js/pages/about/AboutPage.jsx
@@ -1,6 +1,6 @@
 var React = require("react");
 var DocumentTitle = require("react-document-title");
-var Link = require("react-router").Link;
+var Link = require("react-router-dom").Link;
 
 var AboutPage = React.createClass({
     render: function(){
