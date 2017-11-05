ca459bc9b195f2aa56add2fe820263b11e3df410
hailedev <hai.le@hotmail.com.au>
2017-11-05 02:45:55 -0800

    initial commit

---
+++ HomepageCore.UI/webpack.config.js
@@ -0,0 +1,96 @@
+var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+var outputPath = path.resolve(__dirname, "./wwwroot/");
+var jsOutputPath = outputPath + "/js/";
+var cssOutputPath = outputPath + "/css/";
+var imagesOutputPath = outputPath + "/images/";
+var fontsOutputPath = outputPath + "/fonts/";
+var resumeOutputPath = outputPath + "/resume/";
+
+function ActivateRazorPlugin(options){};
+ActivateRazorPlugin.prototype.apply = function(compiler){
+    compiler.plugin("compilation", function(compilation) {
+        compilation.plugin("html-webpack-plugin-after-html-processing", function(htmlPluginData, callback) {
+            console.log("running");
+            htmlPluginData.html = htmlPluginData.html.replace(/<!-- razor:\s?(.|[\s\S]*?)\s?-->/g, "$1");
+            callback(null, htmlPluginData);
+        });
+    });
+};
+
+module.exports = {
+    context: path.join(__dirname, "./ClientApp"),
+    entry: {
+        "app": ["./js/app", "./css/styles.less"],
+        "prerender-app": ["./js/prerender-app"]
+    },
+    output: {
+        path: outputPath,
+        publicPath: "/",
+        filename: "js/[name].js"
+    },
+    plugins: [
+        new CleanWebpackPlugin([outputPath + "/**/*"]),
+        new ExtractTextPlugin("css/styles.css"),
+        new CopyWebpackPlugin([
+            { from: "./js/prerender-bootstrapper.js", to: jsOutputPath },
+            { from: "./*.css", to: cssOutputPath, context: "./css" },
+            { from: "./**", to: imagesOutputPath, context: "./images" },
+            { from: "./**", to: fontsOutputPath, context: "./fonts" },
+        ]),
+        new CopyWebpackPlugin([
+            { from: "./assets/**", to: resumeOutputPath, context: "./submodules/InteractiveResume/src" },
+            { from: "./styles/**/*", to: resumeOutputPath, context: "./submodules/InteractiveResume/src" },
+            { from: "./js/lib/jquery-1.11.2.min.js", to: resumeOutputPath + "js/lib", context: "./submodules/InteractiveResume/src" },
+            { from: "./js/lib/require.js", to: resumeOutputPath + "js/lib", context: "./submodules/InteractiveResume/src" }
+        ]),
+        new CopyWebpackPlugin([
+            { from: "./assets/**", to: outputPath + "/blizzard/", context: "./submodules/InteractiveResume/src" },
+            { from: "./styles/**/*", to: outputPath + "/blizzard/", context: "./submodules/InteractiveResume/src" },
+            { from: "./js/lib/jquery-1.11.2.min.js", to: outputPath + "/blizzard/js/lib/", context: "./submodules/InteractiveResume/src" },
+            { from: "./js/lib/require.js", to: outputPath + "/blizzard/js/lib/", context: "./submodules/InteractiveResume/src" }
+        ]),
+        new HtmlWebpackPlugin({ 
+            template: "./templates/index.html",
+            inject: false,
+            enableGoogleAnalytics: false
+        }),
+        new ActivateRazorPlugin()
+    ],
+    resolve: {
+        modules: [
+            "node_modules",
+            "./js/constants",
+            "./js/dispatchers",
+            "./js/stores",
+            "./js/actions",
+            "./js/api",
+            "./js/common"
+        ],
+        extensions: [".js", ".jsx"],
+        alias: {
+            "env": path.resolve(__dirname, "./appsettings.Development.json")
+        }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react"]
+                }
+            },
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract("css-loader!less-loader")
+            }
+        ]
+    }
+};
