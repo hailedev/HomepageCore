bdbece65c7c373d3694676fe12f85de07be2d3bf
hailedev <hai.le@hotmail.com.au>
2017-11-11 19:53:01 -0800

    fix up shell script

--- go.sh
+++ go.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+menu()
+{
+    echo "publish        Publish the application to ./Target"
+    echo "build-image    Build the docker image"
+    echo "run            Run the application in a docker container"
+    echo "help           Show this menu"
+    echo ""
+}
+
 if [ "$1" = "publish" ] 
 then
     dotnet publish ./HomepageCore.UI/HomepageCore.UI.csproj -c Release -o ../Target
+elif [ "$1" = "build-image" ]
+then
+    docker build -t hailedev/homepagecore .
+elif [ "$1" = "run" ]
+then
+    docker run --rm -it -p 8080:80 hailedev/homepagecore
+else
+    menu
 fi
