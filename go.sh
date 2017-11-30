59fd99ebc11cf57a8ae373a5cb44325912d22426
hailedev <hai.le@hotmail.com.au>
2017-11-30 00:32:06 -0800

    parameterize image name

--- go.sh
+++ go.sh
@@ -101,7 +101,7 @@ then
     then
         aws ssm delete-document --name "DeployStack"
     else
-        aws ssm send-command --instance-ids "${INSTANCE}" --document-name "DeployStack" --comment "Deploying build ${VERSION}" --parameters stackfile=https://raw.githubusercontent.com/hailedev/homepagecore/master/deploy/homepagecore.yml
+        aws ssm send-command --instance-ids "${INSTANCE}" --document-name "DeployStack" --comment "Deploying build ${VERSION}" --parameters stackfile="https://raw.githubusercontent.com/hailedev/homepagecore/master/deploy/homepagecore.yml",image="hailedev/homepagecore"
     fi
 else
     menu
