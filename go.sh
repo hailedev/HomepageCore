ddd26bb51ff32c658a049e67f74d37ed06199a4e
hailedev <hai.le@hotmail.com.au>
2018-06-04 04:13:24 -0700

    add nginx config to ssm command

--- go.sh
+++ go.sh
@@ -102,7 +102,7 @@ then
     then
         aws ssm delete-document --name "DeployStack"
     else
-        aws ssm send-command --instance-ids "${INSTANCE}" --document-name "DeployStack" --comment "Deploying build ${VERSION}" --parameters stackfile="https://raw.githubusercontent.com/hailedev/homepagecore/master/deploy/homepagecore.yml",configfile="https://raw.githubusercontent.com/hailedev/homepagecore/master/deploy/default.conf",image="hailedev/homepagecore"
+        aws ssm send-command --instance-ids "${INSTANCE}" --document-name "DeployStack" --comment "Deploying build ${VERSION}" --parameters stackfile="https://raw.githubusercontent.com/hailedev/homepagecore/master/deploy/homepagecore.yml",configfile="https://raw.githubusercontent.com/hailedev/homepagecore/master/deploy/default.conf",nginxfile="https://raw.githubusercontent.com/hailedev/homepagecore/master/deploy/nginx.conf"
     fi
 else
     menu
